b2c89c3767f150ff85cfc26d763def9307948a1b
Artem Sitnikov <a.p.sitnikov@gmail.com>
2018-11-11 22:54:58 +0700

    add comment

--- src/main/java/ru/usque/pelican/entities/PelicanCategory.java
+++ src/main/java/ru/usque/pelican/entities/PelicanCategory.java
@@ -19,22 +19,40 @@ public class PelicanCategory implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private long id;
+
     @Column(name = "user_id")
     private Integer userId;
+
     @Column(name = "category_parent_id")
     private Integer categoryParentId;
+
     @Column(name = "name")
     private String name;
+
+    // простая? (выполнение булевое) - планка.
     @Column(name = "simple")
     private Boolean simple;
+
+    // % в день от планки ?
     @Column(name = "score")
     private Integer score;
+
+    // имеет ли лимит (книга)
     @Column(name = "disposable")
     private Boolean disposable;
+
+    // total limit
     @Column(name = "disposable_capacity")
     private Integer disposableCapacity;
+
+    // сколько сделано из
     @Column(name = "disposable_done")
     private Integer disposableDone;
+
+    //задача выполнена?
     @Column(name = "deprecated")
     private Boolean deprecated;
+
+    @Column(name = "header")
+    private Boolean header;
 }
